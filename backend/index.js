e72299c213414c5770b03abda2f72df9f8f7cfe0
Albert <albert.barsamov@gmail.com>
2022-01-14 15:39:17 +0100

    first server deployement n 5000port

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const express = require('express')
+
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+
+const start = () => {
+    try {
+
+
+        app.listen(PORT, () => {
+            console.log(`server is deployed on port ${PORT}`)
+        })
+    } catch (e) {
+
+    }
+}
+
+start()
