6dcd6e2d2dfae7ddbb3729c377b225210dfea3f7
Albert <albert.barsamov@gmail.com>
2022-01-14 16:58:42 +0100

    connecting to db with process env

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
 const express = require('express')
-
 const mongoose = require('mongoose')
 const config = require('config')
+require('dotenv').config()
+
 
 const app = express()
 const PORT = config.get('serverPort')
 
 const start = async () => {
     try {
-        await mongoose.connect(config.get("dbUrl"))
+        await mongoose.connect(process.env.REACT_APP_MONGODB_URL)
 
         app.listen(PORT, () => {
             console.log(`server is deployed on port ${PORT}`)
+            // console.log(process.env.REACT_APP_MONGODB_URL)
         })
     } catch (e) {
-
+        console.log(e)
     }
 }
 
