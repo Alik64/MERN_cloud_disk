4aca28e2e41f8e63344751f4e443c1e210386ba1
Albert <albert.barsamov@gmail.com>
2022-01-17 15:04:51 +0100

    npm i redux react-redux redux-thunk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,56 +3,11 @@ import axios from 'axios'
 
 function App() {
 
-  const [email, setEmail] = useState("")
-  const [pwd, setPwd] = useState("")
 
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (email && pwd) {
-      const newValues = JSON.stringify({ email: email, password: pwd })
-      axios.post("http://localhost:5000/api/auth/registration", newValues, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-
-    }
-
-
-
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
-  const handlePwdChange = (e) => {
-    setPwd(e.target.value)
-  }
   return (
     <div className="App">
 
-      // http://localhost:5000/api/auth/registration
-
-      <form onSubmit={handleSubmit} >
-        <input
-          value={email}
-          onChange={handleEmailChange}
-          placeholder='Email'
-          type="email"
-          required />
-        <br />
-        <input
-          value={pwd}
-          onChange={handlePwdChange}
-          placeholder='Password'
-          type="password"
-          required />
-
-        <br />
-        <button>Submit</button>
-      </form>
+      hello world
     </div>
   );
 }
