210b85d1f46f0daaf20a66d6c9b88ee5cad687e5
Albert <albert.barsamov@gmail.com>
2022-01-28 00:21:36 +0100

    Download files + css

--- frontend/src/components/Disk/fileList/File/File.jsx
+++ frontend/src/components/Disk/fileList/File/File.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './File.css'
 import folder from '../../../../assets/images/folder.png'
+import garbage from '../../../../assets/images/garbage.png'
+import download from '../../../../assets/images/download.png'
 import docs from '../../../../assets/images/file.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { pushToStack, setCurrentDir } from '../../../../redux/fileReducer'
+import { downloadFile } from '../../../../actions/file'
 
 export default function File({ file }) {
     const dispatch = useDispatch()
@@ -16,7 +19,10 @@ export default function File({ file }) {
         }
 
     }
-
+    const downloadHandler = (e) => {
+        e.stopPropagation()
+        downloadFile(file)
+    }
     return (
         <div className='File' onClick={() => openFolderHandler(file)}>
             <img
@@ -25,8 +31,13 @@ export default function File({ file }) {
                 alt="type" />
 
             <div className='File_name'>{file.name}</div>
+
+            {file.type !== 'dir' && <img src={download} alt="download" title='Download'
+                className='File_download File_ico' onClick={(e) => downloadHandler(e)} />}
+            <img src={garbage} alt="delete" title='Delete' className='File_delete File_ico' />
             <div className='File_date'>{file.date.slice(0, 10)}</div>
             <div className='File_size'>{file.size} </div>
+
             {/* <div className='File_size'>{parseFloat(file.size / 1048576).toFixed(2)} Mb</div> */}
         </div>
     )
