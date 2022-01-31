ac67b5744b7a2623c574bfff22bc3736b2d6e00d
Albert <albert.barsamov@gmail.com>
2022-01-31 10:56:23 +0100

    create uploadFile.jsx + css

---
+++ frontend/src/components/Disk/fileList/Uploader/UplodFile/UploadFile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UploadFile.css'
+
+export default function UploadFile(file) {
+
+    return (
+        <div className='UploadFile'>
+            <div className='UploadFile_header'>
+                <div></div>
+                <div>
+                    <div>FILE</div>
+                </div>
+            </div>
+        </div>)
+}
