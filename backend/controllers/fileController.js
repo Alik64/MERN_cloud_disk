836bc63dc885565b5d98229c81ba1bc778b8b4da
Albert <albert.barsamov@gmail.com>
2022-02-21 13:08:07 +0100

    create fileroute + upload + delete avatar - backend

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -1,9 +1,11 @@
-const fileService = require('../services/fileService')
+
 const config = require('config')
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+
 const User = require('../models/User')
 const File = require('../models/File')
-
+const fileService = require('../services/fileService')
 
 
 class FileController {
@@ -158,6 +160,36 @@ class FileController {
         } catch (e) {
             console.log(e)
             return res.status(400).json({ message: 'Search error' })
+        }
+    }
+
+    async uploadAvatar(req, res) {
+        try {
+            const file = req.files.file
+            const user = await User.findById(req.user.id)
+            const avatarName = uuidv4() + ".jpg";
+            file.mv(config.get('staticPath') + "/" + avatarName)
+            user.avatar = avatarName
+            await user.save()
+            return res.json(user)
+
+        } catch (e) {
+            console.log(e)
+            return res.status(400).json({ message: 'Upload avatar error' })
+        }
+    }
+    async deleteAvatar(req, res) {
+        try {
+
+            const user = await User.findById(req.user.id)
+            fs.unlinkSync(config.get('staticPath') + "/" + user.avatar)
+            user.avatar = null
+            await user.save()
+            return res.json(user)
+
+        } catch (e) {
+            console.log(e)
+            return res.status(400).json({ message: 'Delete avatar error' })
         }
     }
 }
