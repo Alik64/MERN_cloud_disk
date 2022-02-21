bb92c6e3899cfdc10d2f57d1c4d5ecf0bfa4495b
Albert <albert.barsamov@gmail.com>
2022-02-21 22:44:01 +0100

    use navigate after Registration

--- frontend/src/components/Registration/Registration.jsx
+++ frontend/src/components/Registration/Registration.jsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react'
 import Input from '../../utils/Input/Input'
 import style from './Registration.module.css'
 import { registration } from '../../actions/user'
+import { useNavigate } from 'react-router-dom'
 
 export default function Registration() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function registrationHandler() {
+        registration(email, password)
+        navigate('/')
+    }
+
+
     return (
         <div className={style.registration}>
 
@@ -14,7 +24,7 @@ export default function Registration() {
                 <Input value={email} setValue={setEmail} type="email" placeholder="E-mail" />
                 <Input value={password} setValue={setPassword} type="password" placeholder="Password" />
             </form>
-            <button onClick={() => registration(email, password)} className='btn'>Let's go!</button>
+            <button onClick={() => registrationHandler()} className='btn'>Let's go!</button>
 
         </div>
     )
